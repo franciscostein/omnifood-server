eaf838a2c46c989122b59cb1b44c1e8cb73cf2ae
Leandro Costa <leandro.franc2008@hotmail.com>
2021-03-08 15:17:34 -0300

    added all files

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 // setting middleware
 app.use(express.static(__dirname + 'public'));  // serves resources from public folder
 
-const server = app.listen();
+const server = app.listen(5000);
